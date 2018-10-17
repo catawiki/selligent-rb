1d64eed3eeab4741f194ccb93fe5cc5b1cdd40f1
Werner Hofstra <w.hofstra@catawiki.nl>
2018-10-17 16:18:40 +0200

    Basic connection

---
+++ lib/selligent/connection.rb
@@ -0,0 +1,22 @@
+module Selligent
+  # Network layer
+  module Connection
+    def get(url, options = {})
+      connection.get url, options
+    end
+
+    def post
+      connection.post url, options
+    end
+
+    def put
+      connection.put url, options
+    end
+
+    private
+
+    def connection
+      @connection ||= Faraday.new(url: host)
+    end
+  end
+end
