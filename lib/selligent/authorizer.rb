6f0f9ced55ecf887436480c3eb57821306ce1db0
Werner Hofstra <w.hofstra@catawiki.nl>
2018-10-19 16:45:46 +0200

    Make auth header generation more OO

--- lib/selligent/authorizer.rb
+++ lib/selligent/authorizer.rb
@@ -4,10 +4,18 @@ require 'openssl'
 
 module Selligent
   # Implements the authorization header for Selligent requests
-  module Authorization
+  class Authorizer
+    attr_reader :config, :time
+    private :config, :time
+
+    def initialize(config, time: Time)
+      @config = config
+      @time = time
+    end
+
     def auth_header(env)
-      timestamp = Time.now.to_i
-      data = "#{timestamp}-#{env.method}-#{env.url}"
+      timestamp = time.now.to_i
+      data = "#{timestamp}-#{env[:method]}-#{env[:url]}"
       hash_code = OpenSSL::HMAC.hexdigest('sha256', config.api_key, data)
 
       "hmac #{config.username}:#{hash_code}:#{timestamp}"
--- lib/selligent/client.rb
+++ lib/selligent/client.rb
@@ -1,6 +1,8 @@
 # frozen_string_literal: true
 
-require 'selligent/authorization'
+require 'forwardable'
+
+require 'selligent/authorizer'
 require 'selligent/configuration'
 require 'selligent/connection'
 require 'selligent/middlewares/authorization'
@@ -8,14 +10,18 @@ require 'selligent/middlewares/authorization'
 module Selligent
   # The actual Selligent client
   class Client
-    include Selligent::Authorization
+    extend Forwardable
+
     include Selligent::Connection
 
     attr_reader :config
 
+    def_delegators :@authorizer, :auth_header
+
     def initialize(options = {})
       Selligent::Middlewares::Authorization.setup!
       @config = Selligent::Configuration.new(options)
+      @authorizer = Selligent::Authorizer.new(@config)
     end
 
     def configure
