1f4d05c39b6bdd440324c15172047f1d41410eaa
Trykov Pavel <ptrykov@gmail.com>
2018-10-22 16:55:16 +0200

    Use timecop for time mocking (#5)
    * Remove time paramtere from Authorizer
    
    * Update specs
    
    * test

--- spec/selligent/authorizer_spec.rb
+++ spec/selligent/authorizer_spec.rb
@@ -3,12 +3,15 @@ require 'ostruct'
 
 RSpec.describe Selligent::Authorizer do
   let(:config) { OpenStruct.new(username: 'catawiki', api_key: 'secret') }
-  let(:time) { OpenStruct.new(now: Time.new(2018, 10, 19)) }
   let(:env) { { method: 'get', url: 'http://selligent.com' } }
 
-  subject(:authorizer) { described_class.new(config, time: time) }
+  subject(:authorizer) { described_class.new(config) }
 
   describe '.auth_header' do
+    before do
+      Timecop.freeze(Time.new(2018, 10, 19))
+    end
+
     it 'generates the authorization header' do
       expected = 'hmac catawiki:9a454c90db8ec24825a02bd2c40c5c0e22681612442ec1e2dc93510e0ceb8496:1539900000'
       expect(authorizer.auth_header(env)).to eq(expected)
