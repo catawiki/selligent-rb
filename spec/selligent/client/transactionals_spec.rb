1ef0a9e91cbf77410d0925161b5d4d5bfd61ad7c
Werner Hofstra <w.hofstra@catawiki.nl>
2018-10-24 14:59:27 +0200

    Add transactional endpoint specs

---
+++ spec/selligent/client/transactionals_spec.rb
@@ -0,0 +1,107 @@
+require 'spec_helper'
+
+RSpec.describe Selligent::Client::Transactionals do
+  subject(:client) do
+    instance_double(
+      Selligent::Client,
+      get: nil,
+      post: nil,
+      base_url: base_url
+    ).extend(described_class)
+  end
+
+  let(:base_url) { '/Portal/Api/organizations/my-org' }
+  let(:api_name) { 'campaign' }
+
+  describe '.transactionals' do
+    it 'calls the transactional endpoint' do
+      client.transactionals
+
+      expect(client).to have_received(:get).with("#{base_url}/journeys/transactional")
+    end
+  end
+
+  describe '.transactional' do
+    it 'calls the correct endpoint' do
+      client.transactional(api_name)
+
+      expect(client).to have_received(:get).with("#{base_url}/journeys/transactional/#{api_name}")
+    end
+  end
+
+  describe '.send_transactional' do
+    let(:model) do
+      {
+        items: [
+          email: 'info@example.com',
+          language: 'EN',
+          data: {},
+          dtsdata: []
+        ]
+      }
+    end
+
+    it 'calls the correct endpoint' do
+      client.send_transactional(api_name, model)
+
+      expect(client).to have_received(:post).with(
+        "#{base_url}/journeys/transactional/#{api_name}/send",
+        model
+      )
+    end
+  end
+
+  describe 'update_profile_and_send_transactional' do
+    let(:model) do
+      {
+        items: [
+          email: 'info@example.com',
+          language: 'EN',
+          profile: [
+            scope: 'MASTER',
+            fields: {
+              'Name' => 'John',
+              'LastName' => 'Smith'
+            }
+          ],
+          data: {},
+          dtsdata: []
+        ]
+      }
+    end
+
+    it 'calls the correct endpoint' do
+      client.update_profile_and_send_transactional(api_name, model)
+
+      expect(client).to have_received(:post).with(
+        "#{base_url}/journeys/transactional/#{api_name}/updateProfileAndSend",
+        model
+      )
+    end
+  end
+
+  describe 'transactionals_status' do
+    let(:message_ids) { %w[123 234] }
+
+    it 'calls the correct endpoint' do
+      client.transactionals_status(message_ids)
+
+      expect(client).to have_received(:post).with(
+        "#{base_url}/journeys/transactional/status",
+        message_ids
+      )
+    end
+  end
+
+  describe 'transactional_status' do
+    let(:message_id) { '123' }
+
+    it 'calls the correct endpoint' do
+      client.transactional_status(message_id)
+
+      expect(client).to have_received(:get).with(
+        "#{base_url}/journeys/transactional/status/#{message_id}"
+      )
+    end
+  end
+end
