8c566941172172ff2db0d3975898e36e92d90c25
Alexei Maridashvili <alexei.maridashvili@gmail.com>
2018-10-25 16:00:41 +0200

    Add parameters description

--- lib/selligent/client/data.rb
+++ lib/selligent/client/data.rb
@@ -7,33 +7,48 @@ module Selligent
     # /organizations/:organization/lists/data/*
     module Data
       # Deletes data from the list with the given api-name in the given organization
+      #
+      # @param api_name [String] The api name
       def delete_data(api_name)
         delete "#{base_url}/lists/#{api_name}/data"
       end
 
       # Loads data into the list with the given api-name in the given organization
+      #
+      # @param api_name [String] The api name
       def load_data(api_name)
         post "#{base_url}/lists/#{api_name}/data/load"
       end
 
       # Method to search for data records in a specific table
+      #
+      # @param api_name [String] The api name
       def search_data(api_name)
         post "#{base_url}/lists/#{api_name}/data/search"
       end
 
       # Deletes data via json from the segment with the given segment-api-name in the list
       # with the given api-name in the given organization
+      #
+      # @param api_name [String] The api name
+      # @param segment_api_name [String] API name of the segment
       def delete_data_from_segment(api_name, segment_api_name)
         delete "#{base_url}/lists/#{api_name}/segments/#{segment_api_name}/data"
       end
 
       # Loads data into the segment with the given segment-api-name in the list
       # with the given api-name in the given organization
+      #
+      # @param api_name [String] The api name
+      # @param segment_api_name [String] API name of the segment
       def load_data_into_segment(api_name, segment_api_name)
         post "#{base_url}/lists/#{api_name}/segments/#{segment_api_name}/data/load"
       end
 
       # Method to search for data records within a segment of specific table
+      #
+      # @param api_name [String] The api name
+      # @param segment_api_name [String] API name of the segment
       def search_data_within_segment(api_name, segment_api_name)
         post "#{base_url}/lists/#{api_name}/segments/#{segment_api_name}/data/search"
       end
