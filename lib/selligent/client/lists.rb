caae8f6468c8adc912535a641b5f95ebf65f84d3
Werner Hofstra <w.hofstra@catawiki.nl>
2018-10-25 12:54:50 +0200

    Add scaffolding

---
+++ lib/selligent/client/lists.rb
@@ -0,0 +1,11 @@
+# frozen_string_literal: true
+
+module Selligent
+  class Client
+    # Implements the list endpoints
+    #
+    # /organizations/:organization/lists/*
+    module Lists
+    end
+  end
+end
