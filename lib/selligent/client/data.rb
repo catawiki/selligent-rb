c8fd857a1b92978cfa16d5ffa0b1ce687ed68a46
Alexei Maridashvili <alexei.maridashvili@gmail.com>
2018-10-26 10:38:23 +0200

    Add missed parameters to endpoints comments

--- lib/selligent/client/data.rb
+++ lib/selligent/client/data.rb
@@ -9,22 +9,36 @@ module Selligent
       # Deletes data from the list with the given api-name in the given organization
       #
       # @param api_name [String] The api name
-      def delete_data(api_name)
-        delete "#{base_url}/lists/#{api_name}/data"
+      # @param keyFields [String] Comma-separated list of the key fields to delete records
+      # @option options [String] :fields Comma-separated list of the fields in data records
+      # @option options [String] :mode Data transfer mode (Sync or Stream)
+      # @option options [String] :errorHandling Error handling options
+      def delete_data(api_name, options = {})
+        delete "#{base_url}/lists/#{api_name}/data", options
       end
 
       # Loads data into the list with the given api-name in the given organization
       #
       # @param api_name [String] The api name
-      def load_data(api_name)
-        post "#{base_url}/lists/#{api_name}/data/load"
+      # @param keyFields [String] Comma-separated list of the key fields to create/update records
+      # @option options [String] :fields Comma-separated list of the fields in data records
+      # @option options [String] :mode Data transfer mode (Sync or Stream)
+      # @option options [String] :deduplication De-duplication option during an import data
+      # @option options [String] :data_import_option Data import option during an import data
+      # @option options [String] :create_segment_name Segment name at the time of creation
+      # @option options [String] :create_segment_api_name Segment api name at the time of creation
+      # @option options [String] :errorHandling Error handling options
+      def load_data(api_name, options = {})
+        post "#{base_url}/lists/#{api_name}/data/load", options
       end
 
       # Method to search for data records in a specific table
       #
       # @param api_name [String] The api name
-      def search_data(api_name)
-        post "#{base_url}/lists/#{api_name}/data/search"
+      # @param request  [String] Details on the data that should be searched
+      # @option options [String] :mode Data transfer mode (Sync or Stream)
+      def search_data(api_name, options = {})
+        post "#{base_url}/lists/#{api_name}/data/search", options
       end
 
       # Deletes data via json from the segment with the given segment-api-name in the list
@@ -32,8 +46,12 @@ module Selligent
       #
       # @param api_name [String] The api name
       # @param segment_api_name [String] API name of the segment
-      def delete_data_from_segment(api_name, segment_api_name)
-        delete "#{base_url}/lists/#{api_name}/segments/#{segment_api_name}/data"
+      # @param keyFields [String] Comma-separated list of the key fields to delete records
+      # @option options [String] :fields Comma-separated list of the fields in data records
+      # @option options [String] :mode Data transfer mode (Sync or Stream)
+      # @option options [String] :errorHandling Error handling options
+      def delete_data_from_segment(api_name, segment_api_name, options = {})
+        delete "#{base_url}/lists/#{api_name}/segments/#{segment_api_name}/data", options
       end
 
       # Loads data into the segment with the given segment-api-name in the list
@@ -41,16 +59,23 @@ module Selligent
       #
       # @param api_name [String] The api name
       # @param segment_api_name [String] API name of the segment
-      def load_data_into_segment(api_name, segment_api_name)
-        post "#{base_url}/lists/#{api_name}/segments/#{segment_api_name}/data/load"
+      # @param keyFields [String] Comma-separated list of the key fields to create/update records
+      # @option options [String] :fields Comma-separated list of the fields in data records
+      # @option options [String] :mode Data transfer mode (Sync or Stream)
+      # @option options [String] :deduplication De-duplication option during an import data
+      # @option options [String] :errorHandling Error handling options
+      def load_data_into_segment(api_name, segment_api_name, options = {})
+        post "#{base_url}/lists/#{api_name}/segments/#{segment_api_name}/data/load", options
       end
 
       # Method to search for data records within a segment of specific table
       #
       # @param api_name [String] The api name
       # @param segment_api_name [String] API name of the segment
-      def search_data_within_segment(api_name, segment_api_name)
-        post "#{base_url}/lists/#{api_name}/segments/#{segment_api_name}/data/search"
+      # @param request [String] details on the data that should be searched
+      # @option options [String] :mode Data transfer mode (Sync or Stream)
+      def search_data_within_segment(api_name, segment_api_name, options = {})
+        post "#{base_url}/lists/#{api_name}/segments/#{segment_api_name}/data/search", options
       end
     end
   end
--- spec/selligent/client/data_spec.rb
+++ spec/selligent/client/data_spec.rb
@@ -10,56 +10,72 @@ RSpec.describe Selligent::Client::Data do
   let(:segment_api_name) { 'campaign_segment' }
 
   describe '.delete_data' do
-    it 'calls delete_data endpoint' do
-      client.delete_data(api_name)
+    let(:options) { { mode: 'Stream' } }
 
-      expect(client).to have_received(:delete).with("#{base_url}/lists/#{api_name}/data")
+    it 'calls delete_data endpoint' do
+      client.delete_data(api_name, options)
+
+      expect(client).to have_received(:delete).with("#{base_url}/lists/#{api_name}/data", options)
     end
   end
 
   describe '.load_data' do
-    it 'calls load_data endpoint' do
-      client.load_data(api_name)
+    let(:options) { { deduplication: 'ImportFirstMatch' } }
 
-      expect(client).to have_received(:post).with("#{base_url}/lists/#{api_name}/data/load")
+    it 'calls load_data endpoint' do
+      client.load_data(api_name, options)
+
+      expect(client)
+        .to have_received(:post)
+        .with("#{base_url}/lists/#{api_name}/data/load", options)
     end
   end
 
   describe '.search_data' do
-    it 'calls search_data endpoint' do
-      client.search_data(api_name)
+    let(:options) { { mode: 'Stream' } }
 
-      expect(client).to have_received(:post).with("#{base_url}/lists/#{api_name}/data/search")
+    it 'calls search_data endpoint' do
+      client.search_data(api_name, options)
+
+      expect(client)
+        .to have_received(:post)
+        .with("#{base_url}/lists/#{api_name}/data/search", options)
     end
   end
 
   describe '.delete_data_from_segment' do
+    let(:options) { { mode: 'Stream' } }
+
     it 'calls delete_data_from_segment endpoint' do
-      client.delete_data_from_segment(api_name, segment_api_name)
+      client.delete_data_from_segment(api_name, segment_api_name, options)
 
       expect(client)
         .to have_received(:delete)
-        .with("#{base_url}/lists/#{api_name}/segments/#{segment_api_name}/data")
+        .with("#{base_url}/lists/#{api_name}/segments/#{segment_api_name}/data", options)
     end
   end
 
   describe '.load_data_into_segment' do
+    let(:options) { { deduplication: 'ImportFirstMatch' } }
+
     it 'calls load_data_into_segment endpoint' do
-      client.load_data_into_segment(api_name, segment_api_name)
+      client.load_data_into_segment(api_name, segment_api_name, options)
 
       expect(client)
         .to have_received(:post)
-        .with("#{base_url}/lists/#{api_name}/segments/#{segment_api_name}/data/load")
+        .with("#{base_url}/lists/#{api_name}/segments/#{segment_api_name}/data/load", options)
     end
   end
 
   describe '.search_data_within_segment' do
+    let(:options) { { mode: 'Stream' } }
+
     it 'calls search_data_within_segment endpoint' do
-      client.search_data_within_segment(api_name, segment_api_name)
+      client.search_data_within_segment(api_name, segment_api_name, options)
 
       expect(client)
         .to have_received(:post)
-        .with("#{base_url}/lists/#{api_name}/segments/#{segment_api_name}/data/search")
+        .with("#{base_url}/lists/#{api_name}/segments/#{segment_api_name}/data/search", options)
     end
   end
 end
