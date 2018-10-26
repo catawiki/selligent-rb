acc9fdda5a61a868d4bbb39b63e5383884248ece
Werner Hofstra <w.hofstra@catawiki.nl>
2018-10-26 12:12:29 +0200

    Spec em

---
+++ spec/selligent/client/cumulio_spec.rb
@@ -0,0 +1,30 @@
+require 'spec_helper'
+
+RSpec.describe Selligent::Client::Cumulio do
+  include_context 'base_client'
+
+  subject(:client) { base_client.extend(described_class) }
+
+  let(:base_url) { '/reporting/cumulio/my-org' }
+
+  before do
+    allow(client).to receive(:config).and_return(OpenStruct.new(organization: 'my-org'))
+  end
+
+  describe '.cumulio_datasets' do
+    it 'calls the correct endpoint' do
+      client.cumulio_datasets
+
+      expect(client).to have_received(:get).with("#{base_url}/datasets")
+    end
+  end
+
+  describe '.cumulio_datasets' do
+    let(:model) { {} }
+    it 'calls the correct endpoint' do
+      client.cumulio_query(model)
+
+      expect(client).to have_received(:post).with("#{base_url}/query", model)
+    end
+  end
+end
